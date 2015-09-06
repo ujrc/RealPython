e876d6e2b4961a0db69a3e6d3df84d146d466734
ujrc <uwjearc@yahoo.com>
2015-09-06 18:31:17 -0500

    RealPythonPart1 Chapter4

---
+++ Chapter4/Chapter4Exercises.py
@@ -0,0 +1,24 @@
+phrase='it is said "you cannot create exeperience ,you must undergo it."'
+print phrase
+my_str="It's good to do more practices."
+print my_str
+
+Phrase="It's important to know the basic.\nIt's hard to dive into advanced materials without having strong \nbasion the subject."
+print Phrase
+
+my_Str="To be able to print a long string the same \
+line while it was written on multiple lines in \
+program.Use single  backslash before you move to next line. "
+
+print my_Str
+
+
+#User input
+#1
+name=raw_input("Enter your name:")
+print "My name is ",name.upper()
+country=raw_input("Enter the name of your country:")
+print "My counrty is ",country.lower()
+
+
+
---
+++ Chapter4/first_letter.py
@@ -0,0 +1,3 @@
+#first_letter.py
+password=raw_input("Tell me your password: ")
+print "The first letter you entered was :",password[0].upper()
