cbfbf721aa7b5ec110ceaf80df9771e7f98d543d
ujrc <uwjearc@yahoo.com>
2015-09-06 18:23:48 -0500

    RealPythonPart1 exercises in chapter 2

---
+++ Chapter2/Chapter2Exercises.py
@@ -0,0 +1,23 @@
+
+#Exercise on errors
+#1
+"""
+my_string= "This script has syntax error"
+print my_string)
+
+
+#2
+
+my_text="this script has runtime error
+print my_text
+"""
+# Save code in a script and run it
+
+print "Use print to display text on screen. "
+
+Text="This string is stored in a variable and will be displayed after using print"
+
+print Text
+
+
+
