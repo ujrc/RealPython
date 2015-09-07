e2281b211e0b9cd95393e558fb5ef31d4060f5ff
ujrc <uwjearc@yahoo.com>
2015-09-06 19:04:50 -0500

    Update Chapter4Exercises.py

--- Chapter4/Chapter4Exercises.py
+++ Chapter4/Chapter4Exercises.py
@@ -1,22 +1,38 @@
+ # a srting with double quotation marks
 phrase='it is said "you cannot create exeperience ,you must undergo it."'
 print phrase
+#a string with  an apostrophe
 my_str="It's good to do more practices."
 print my_str
-
+#Display a string on multiple lines
 Phrase="It's important to know the basic.\nIt's hard to dive into advanced materials without having strong \nbasion the subject."
 print Phrase
-
+# Display one-line string written on multiple lines
 my_Str="To be able to print a long string the same \
 line while it was written on multiple lines in \
 program.Use single  backslash before you move to next line. "
 
 print my_Str
 
+# length of a string
+my_string ="Display the length of this string"
+print len(my_string)
+# concatenate two strings
+print "Learn and use "+"Python"
+#concatenate two strings in two variable
+string1=" If you want to be good in programming,"
+string2="do more practice"
+print string1+" "+string2 
+
+#use indexes to print parof a string
+String="bazinga"
+print String[2:6]
 
 #User input
 #1
 name=raw_input("Enter your name:")
 print "My name is ",name.upper()
+#2
 country=raw_input("Enter the name of your country:")
 print "My counrty is ",country.lower()
 
